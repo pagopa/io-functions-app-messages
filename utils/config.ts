d4db0493e05675d186487e0857713644fcfc0038
AleDore <alessio85.dore@gmail.com>
2022-03-21 21:33:43 +0100

    restore boolean for default purpose

--- utils/config.ts
+++ utils/config.ts
@@ -8,6 +8,7 @@
 import * as t from "io-ts";
 
 import * as E from "fp-ts/lib/Either";
+import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/function";
 
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
@@ -16,7 +17,6 @@ import {
   IntegerFromString,
   NonNegativeInteger
 } from "@pagopa/ts-commons/lib/numbers";
-import { BooleanFromString } from "@pagopa/ts-commons/lib/booleans";
 
 // exclude a specific value from a type
 // as strict equality is performed, allowed input types are constrained to be values not references (object, arrays, etc)
@@ -54,10 +54,10 @@ export const RedisParams = t.intersection([
     REDIS_URL: NonEmptyString
   }),
   t.partial({
-    REDIS_CLUSTER_ENABLED: BooleanFromString,
+    REDIS_CLUSTER_ENABLED: t.boolean,
     REDIS_PASSWORD: NonEmptyString,
     REDIS_PORT: NonEmptyString,
-    REDIS_TLS_ENABLED: BooleanFromString
+    REDIS_TLS_ENABLED: t.boolean
   })
 ]);
 export type RedisParams = t.TypeOf<typeof RedisParams>;
@@ -85,16 +85,14 @@ export const IConfig = t.intersection([
 const errorOrConfig: t.Validation<IConfig> = IConfig.decode({
   ...process.env,
   REDIS_CLUSTER_ENABLED: pipe(
-    process.env.REDIS_CLUSTER_ENABLED,
-    BooleanFromString.decode,
-    E.mapLeft(() => false),
-    E.toUnion
+    O.fromNullable(process.env.REDIS_CLUSTER_ENABLED),
+    O.map(_ => _.toLowerCase() === "true"),
+    O.toUndefined
   ),
   REDIS_TLS_ENABLED: pipe(
-    process.env.REDIS_TLS_ENABLED,
-    BooleanFromString.decode,
-    E.mapLeft(() => true),
-    E.toUnion
+    O.fromNullable(process.env.REDIS_TLS_ENABLED),
+    O.map(_ => _.toLowerCase() === "true"),
+    O.toUndefined
   ),
   SERVICE_CACHE_TTL_DURATION: pipe(
     process.env.SERVICE_CACHE_TTL_DURATION,

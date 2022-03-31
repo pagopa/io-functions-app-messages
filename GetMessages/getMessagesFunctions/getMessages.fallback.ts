5f1fb540f8cb7e310fdd7efd3d1641fbb0c63a10
gquadrati <greta.quadrati@pagopa.it>
2022-03-31 16:37:01 +0200

    [#IC-298] add time_to_live and has_attachment to fallback result

--- GetMessages/getMessagesFunctions/getMessages.fallback.ts
+++ GetMessages/getMessagesFunctions/getMessages.fallback.ts
@@ -70,7 +70,15 @@ export const getMessagesFromFallback = (
     messageModel.findMessages(fiscalCode, pageSize, maximumId, minimumId),
     TE.map(i => mapAsyncIterator(i, A.rights)),
     TE.map(i => mapAsyncIterator(i, A.filter(RetrievedNotPendingMessage.is))),
-    TE.map(i => mapAsyncIterator(i, A.map(retrievedMessageToPublic))),
+    TE.map(i =>
+      mapAsyncIterator(
+        i,
+        A.map(m => ({
+          ...retrievedMessageToPublic(m),
+          time_to_live: m.timeToLiveSeconds
+        }))
+      )
+    ),
     TE.chainW(i =>
       // check whether we should enrich messages or not
       pipe(

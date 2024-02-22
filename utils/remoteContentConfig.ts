a3e8319f1bf835f17593b76efacd03186b1b429b
Michael Disaro <michael.disaro@gmail.com>
2024-02-22 14:47:24 +0100

    fixed linter

--- utils/remoteContentConfig.ts
+++ utils/remoteContentConfig.ts
@@ -30,6 +30,7 @@ export default class RCConfigurationUtility {
    * or else it tries to get the configuration id by service id from the related map.
    * If the configuration is cached it retrieves it from cache, or else it retrieves
    * it from cosmosdb and provides to cache it.
+   *
    * @param serviceId
    * @param configurationId
    * @returns
@@ -62,6 +63,7 @@ export default class RCConfigurationUtility {
    * This method is used to get a configuration, if it exists, by configuratin id.
    * If the configuration is cached it retrieves it from cache, or else it retrieves
    * it from cosmosdb and provides to cache it.
+   *
    * @param configurationId
    * @returns
    */
